21e193bc405b42ba84bd7a0cc79d8705d43beb5e
Adi <adielrontw@gmail.com>
2025-11-26 18:04:38 +0200

    Managed OpenAi API Key

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello via Bun!");
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config() ;
+
+
+
+import type {Request, Response} from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.send("Hello, World!");
+});
